1734567f32846ad2e06f7927ca3f6485efdd0b0e
soxft <email@xcsoft.top>
2022-07-10 22:04:13 +0800

    add unit test

---
+++ library/mail/struct_test.go
@@ -0,0 +1,15 @@
+package mail
+
+import (
+	"log"
+	"reflect"
+	"testing"
+)
+
+func TestStruct(t *testing.T) {
+	r := reflect.TypeOf(GuestArgs{})
+	for i := 0; i < r.NumField(); i++ {
+		field := r.Field(i)
+		log.Println(field.Name, field.Tag.Get("args"))
+	}
+}
