aac4dc2a6251de0709557bbfbacf918ba06afd7a
soxft <email@xcsoft.top>
2022-07-13 11:53:12 +0800

    fix

--- templates/templates.go
+++ templates/templates.go
@@ -14,10 +14,10 @@ var (
 
 func init() {
 	// get file content
-	if _ownerTemplate, err := ioutil.ReadFile("./templates/owner.html"); err != nil {
+	if _ownerTemplate, err := ioutil.ReadFile("./templates/owner.html"); err == nil {
 		Owner = string(_ownerTemplate)
 	}
-	if _guestTemplate, err := ioutil.ReadFile("./templates/guest.html"); err != nil {
+	if _guestTemplate, err := ioutil.ReadFile("./templates/guest.html"); err == nil {
 		Guest = string(_guestTemplate)
 	}
 }
