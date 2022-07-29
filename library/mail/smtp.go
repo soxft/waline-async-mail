5c2c26b964f016a8e8c7effaf5078c2a9420d311
soxft <email@xcsoft.top>
2022-07-29 12:00:00 -0500

    optizmes

--- library/mail/smtp.go
+++ library/mail/smtp.go
@@ -11,7 +11,6 @@ import (
 func sendBySmtp(mail Mail) error {
 	m := gomail.NewMessage()
 
-	// go-gomail/gomail/issues/155
 	senderNameUtf8 := mime.QEncoding.Encode("utf-8", config.Smtp.SenderName)
 	m.SetHeader("From", fmt.Sprintf("\"%s\" <%s>", senderNameUtf8, config.Smtp.SenderEmail)) // 发件人
 	m.SetHeader("To", mail.ToAddress)                                                        // 收件人
